b7e45819d6d7511398d4c9bc64ff347bb0ca973b
bukhassan <bukhassan20@gmail.com>
2019-01-04 20:23:24 +0100

    exos.Jeudi

---
+++ 08.rb
@@ -0,0 +1,4 @@
+puts "what is your name?"
+print">"
+user_name=gets.chomp
+puts"bonjour, #{user_name}!"
---
+++ 09.rb
@@ -0,0 +1,5 @@
+puts "salut ! quel est ton prénom ?"
+user_name = gets.chomp
+puts ">"
+user_surname = gets.chomp
+puts "Bonjour #{user_name}#{user_surname} !"
